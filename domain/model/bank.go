33f26ea9298b978d65f20cff141ee459b001e2cd
luiz <luizgustavofcsg@hotmail.com>
2022-05-04 12:06:00 -0300

    Basic model for bank definition

---
+++ domain/model/bank.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type Bank struct {
+	ID        string    `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (bank *Bank) isValid() error {
+	return nil
+}
+
+func newBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	bank.UpdatedAt = time.Now()
+
+	err := bank.isValid()
+
+	return &bank, err
+}
